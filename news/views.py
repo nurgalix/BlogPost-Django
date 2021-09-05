c52d0a7abd6f6d3ac7fa98ee3b5ceea5471cd4db
Nurgali Nuraly <nurali.nurgali@gmail.com>
2021-09-05 09:42:19 +0600

    Forms

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from .models import News, Category
+from .forms import NewForm
 
 
 # контроллер(вьюшка) отрабатывает клиентский запрос, запрашивает данные у модели и возвращает ответ в виде представление
@@ -28,3 +29,11 @@ def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {"news_item": news_item})
+
+
+def add_news(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = NewForm()
+    return render(request, 'news/add_news.html', {'form': form})
