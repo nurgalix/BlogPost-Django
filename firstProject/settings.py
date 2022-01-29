00044dd804b7420a675b8f561c95821b1fa5e5af
Nurgali Nuraly <nuralynurgalix@gmail.com>
2022-01-29 16:45:57 +0600

    Installation of Debug Toolbar

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'news.apps.NewsConfig',
 ]
 
@@ -46,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'firstProject.urls'
@@ -130,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+INTERNAL_IPS = ["127.0.0.1"]
--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from news.models import Category
+from django.db.models import Count
 
 register = template.Library()
 
@@ -12,5 +13,7 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    # Выводит рубрики в которой больше 0 записи
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2':arg2}
