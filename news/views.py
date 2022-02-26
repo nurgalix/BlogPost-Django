cc77913ae2eed025600473bb47738cddfb519352
Nurgali Nuraly <nuralynurgalix@gmail.com>
2022-02-26 22:04:03 +0600

    Mixins

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from .models import News, Category
 from .forms import NewsForm
